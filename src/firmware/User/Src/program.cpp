fec2c77848933e57cd0e04fec3bd46a93ec3fd99
John <TPgMNMKQLre897@rs.ru>
2024-05-14 17:11:25 +0300

    Add calculate the capacity

--- src/firmware/User/Src/program.cpp
+++ src/firmware/User/Src/program.cpp
@@ -5,9 +5,6 @@
 #include "usart.h"
 #include "main.h"
 #include "ssd1306.h"
-#include "ssd1306_tests.h"
-#include "ssd1306_fonts.h"
-#include "commands_decoder.h"
 #include "fee.h"
 #include "soft_timers.h"
 #include "stdio.h"
@@ -25,6 +22,10 @@
 #define DISPLAY_CAPACITY 2
 #define DISPLAY_PERCENTS 3
  
+#define DIVISION_COEFFICIENTS_VOLTAGE 4.55
+#define DIVISION_COEFFICIENTS_CURRENT 3.64
+#define MAX_QUANTITY_MEASUREMENTS 15
+
 extern IWDG_HandleTypeDef hiwdg;
 extern TIM_HandleTypeDef htim3;
 extern ADC_HandleTypeDef hadc1;
@@ -32,7 +33,15 @@ extern ADC_HandleTypeDef hadc1;
 volatile uint32_t TimeTickMs = 0;
 uint32_t oldTimeTickHSec = 0;
 
-uint32_t adcData[ADC_NUMBER_OF_CHANNELS]; //Measured adc values. 
+uint32_t adcData[ADC_NUMBER_OF_CHANNELS]; //Measured adc values.
+
+uint32_t adcAvgBuff[2];
+float adcResults[2];
+uint8_t numberMeasurements = 0;
+float calculatedCapacity;
+
+bool secondTimerHandler = false;
+float lastCurrent;
 
 char PassWord_W[10] = {"ABC123xyz"};
 char PassWord_R[9];
@@ -41,19 +50,6 @@ bool logoSwith = false;
 char displayStr[30]; //To display data on the screen.
 bool animationFlag = false; //To flash the animation on the screen
 
-
-// Options for the status of the transmission method.
-enum TxState
-{ 
-  Default = 0, ///< synchronous transmission
-  Interrupt, ///< asynchronous transmission (interrupts)
-  DMA ///< asynchronous transfer (DMA)
-};
-
-// Status of the transmission method.
-TxState State = TxState::Default;
-
-char usartString[USART_STRING_SIZE]; //The string that will be sent via usart.
 /**
  * \brief  Performs initialization. 
  *
@@ -70,7 +66,6 @@ void init( void )
 void setup( void )
 {
     HAL_ADC_Start_DMA(&hadc1, adcData, ADC_NUMBER_OF_CHANNELS); // start adc in DMA mode
-    commands_decoder_init();
 
     // Setting the default state.  
     if ( HAL_GPIO_ReadPin( USER_LED_GPIO_Port, USER_LED_Pin ) == GPIO_PIN_SET )
@@ -81,27 +76,29 @@ void setup( void )
 
    HAL_IWDG_Refresh(&hiwdg); 
   //FEE_Write(FEE_START_ADDRESS, (uint16_t*)PassWord_W, sizeof(char)*9);
-  FEE_Read(FEE_START_ADDRESS, (uint16_t*)PassWord_R, sizeof(char)*9);
-  sprintf(usartString, "Read= %s\r\n", PassWord_R);
-  HAL_UART_Transmit( & UART, ( uint8_t * ) usartString, strlen(usartString), 50 );
-  HAL_Delay(500);
-
-   // uint8_t *buff = flash_read_test();
-     
+  //FEE_Read(FEE_START_ADDRESS, (uint16_t*)PassWord_R, sizeof(char)*9);
+  //sprintf(usartString, "Read= %s\r\n", PassWord_R);
+   // uint8_t *buff = flash_read_test();   
     //HAL_UART_Transmit(&UART, buff, 24, 50);
     //flash_write_test();
 }
 
-void voltageToString(uint32_t adc, char* str)
+float calculatedDC(uint32_t adcValue, bool paramType)
 {
-   float voltage = adc  * ADC_REFERENCE_VOLTAGE / ADC_MAX;
-   sprintf(str,"%d.%02d ", (uint32_t)voltage, (uint16_t)((voltage - (uint32_t)voltage) * 100.));    
+   if(paramType)
+   {
+      return adcValue  * ADC_REFERENCE_VOLTAGE * DIVISION_COEFFICIENTS_VOLTAGE / ADC_MAX;
+   }
+   else
+   {
+      return adcValue  * ADC_REFERENCE_VOLTAGE * DIVISION_COEFFICIENTS_CURRENT / ADC_MAX;
+   }
 }
 
 //Show on display the current or voltage
-void printDisplayParameter(uint16_t data, uint8_t paramType)
+void printDisplayParameter(float data, uint8_t paramType)
 {
-   sprintf(displayStr,"%d.%02d ", (uint16_t)data / 100, (uint16_t)(data % 100));
+   sprintf(displayStr, "%d.%02d ", (uint32_t)data, (uint16_t)((data - (uint32_t)data) * 100.)); 
 
    char* simvol = "В";
 
@@ -122,8 +119,8 @@ void printDisplayParameter(uint16_t data, uint8_t paramType)
     break;
 
     case DISPLAY_PERCENTS:
-      sprintf(displayStr,"%d ", data);
-      ssd1306_SetCursor(90, 0);
+      sprintf(displayStr,"%d ", (uint8_t)data);
+      ssd1306_SetCursor(85, 0);
       
       if(animationFlag)
       {
@@ -144,6 +141,15 @@ void printDisplayParameter(uint16_t data, uint8_t paramType)
    ssd1306_PrintString(simvol, 2);
 }
 
+void calculateCapacity()
+{
+   if(secondTickHandler)
+   {
+     secondTimerHandler = false;
+     calculatedCapacity+= calculatedDC(adcResults[1], false);
+   }
+}
+
 /**
  * \brief   It is performed periodically in the body of the main loop.
  *
@@ -152,65 +158,23 @@ void printDisplayParameter(uint16_t data, uint8_t paramType)
 void loop( void )
 {
     HAL_Delay(1000);
-     //HAL_GPIO_TogglePin( USER_LED_GPIO_Port, USER_LED_Pin);
 
     HAL_IWDG_Refresh(&hiwdg);
 
     //ssd1306_SetCursor(0,0);
     //ssd1306_PrintString("ЗАПУСК..", 2);
-
-    printDisplayParameter(1356, DISPLAY_VOLTAGE);
-    printDisplayParameter(1238, DISPLAY_CURRENT); 
-    printDisplayParameter(12838, DISPLAY_CAPACITY); 
-    printDisplayParameter(98, DISPLAY_PERCENTS); 
-//
-
-     //   ssd1306_WriteString("454", Font_7x10, White);
-     //char *test ="Привет";
-     //char test1[20];
-    //sprintf(test1,"%02X", test[2]);
-    //ssd1306_WriteString(test1, Font_7x10, White);
-    //ssd1306_PrintChar(76);
+    calculateCapacity(); 
+     
+    ssd1306_Fill(Black); 
+    printDisplayParameter(calculatedDC(adcResults[0], true), DISPLAY_VOLTAGE);
+    printDisplayParameter(calculatedDC(adcResults[1], false), DISPLAY_CURRENT); 
+    printDisplayParameter(calculatedCapacity / 3600, DISPLAY_CAPACITY); 
+    printDisplayParameter(100, DISPLAY_PERCENTS); 
+    
    
     ssd1306_UpdateScreen();
-   return;
-
-  if(!needSendDataToUSART()) return ;
-  // We are waiting for the end of the packet transmission.
-  if (UART.gState != HAL_UART_STATE_READY ) return;
-
-  int strPos = 0;
-  for(int i = 0; i < ADC_NUMBER_OF_CHANNELS; i++)
-  {
-    voltageToString(adcData[i], usartString + i * VOLTAGE_STR_LEN + 1);
-    strPos+= VOLTAGE_STR_LEN;
-  }
-
-  HAL_IWDG_Refresh(&hiwdg);
-  sprintf(usartString + strPos + 1, "u\n");
-  HAL_UART_Transmit( & UART, ( uint8_t * ) usartString, sizeof( usartString ), 50 );
 }
 
-void enableBeeper(bool enabled)
-{
-   GPIO_InitTypeDef GPIO_InitStruct = {0};
-   GPIO_InitStruct.Pin = GPIO_PIN_1;
-   
-   if(enabled)
-   {
-     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-   }
-   else
-   {
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_INPUT;
-   }
-   
-   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-}
-
-int beep = 20;
-bool alarm = false;
 void HAL_SYSTICK_Callback( void )
 {
     TimeTickMs++;
@@ -218,35 +182,33 @@ void HAL_SYSTICK_Callback( void )
     if (TimeTickMs - oldTimeTickHSec > 1000)
     {
         oldTimeTickHSec = TimeTickMs;
-        secondTickHandler();
-        // Indication of the main cycle operation.
-      //HAL_GPIO_TogglePin( USER_LED_GPIO_Port, USER_LED_Pin);
-   
+        secondTimerHandler = true;
+        lastCurrent = adcResults[1];        
     }    
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
 {   
    if (htim->Instance == TIM2)
-   {
-    // HAL_GPIO_TogglePin( USER_LED_GPIO_Port, USER_LED_Pin);
-    // Indication of the main cycle operation.
-         if(beep > 30) beep = 10;
+   {      
+      if(numberMeasurements < MAX_QUANTITY_MEASUREMENTS)
+      {
+         for(uint8_t i = 0; i < 2; i++)
+         {
+           adcAvgBuff[i]+= adcData[i]; 
+         }
 
-    beep++;
-//HAL_GPIO_TogglePin( USER_LED_GPIO_Port, USER_LED_Pin);
-      //__HAL_TIM_SET_AUTORELOAD(&htim3, beep*2);
-     // __HAL_TIM_SET_COMPARE(&htim3,TIM_CHANNEL_4, beep);
+         numberMeasurements++;    
+      }
+      else
+      {
+        for(uint8_t i = 0; i < 2; i++)
+         {
+           adcResults[i]= adcAvgBuff[i] / numberMeasurements; 
+           adcAvgBuff[i] = 0;
+         }
 
-    //enableBeeper(alarm);
-    alarm = !alarm;
-
-  
+         numberMeasurements = 0;
+      }
    }
-}
-
-//When all data from usart is received, an interrupt will be triggered.
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-  receive_byte();
 }
