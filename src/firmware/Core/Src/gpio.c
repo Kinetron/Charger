3a2c069a5e769a7d0e831ec9ae350a444d5d9ef9
John <TPgMNMKQLre897@rs.ru>
2024-06-15 19:14:18 +0300

    Add mco pin

--- src/firmware/Core/Src/gpio.c
+++ src/firmware/Core/Src/gpio.c
@@ -50,8 +50,11 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
+  /*Configure GPIO pin : PA8 - MCO */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
